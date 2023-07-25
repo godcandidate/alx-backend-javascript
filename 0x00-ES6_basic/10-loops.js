b10f92ae3a6214580d6781e3348ad23c2e5aab0c
godcandidate <godcandidate@gmail.com>
2023-07-25 21:21:40 +0000

    create files

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,9 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const tempArray = [];
+
+  for (const idx of array) {
+    tempArray.push(appendString + idx);
+  }
+
+  return tempArray;
+}
