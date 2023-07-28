6fa3b562f8171af45c1e519988e16633ba8b8eac
godcandidate <godcandidate@gmail.com>
2023-07-28 11:21:19 +0000

    create files

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,8 +1,10 @@
-// eslint-disable-next-line import/extensions
-import { uploadPhoto, createUser } from './utils.js';
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
-    .then((response) => console.log(`${response[0].body} ${response[1].firstName} ${response[1].lastName}`))
+  return Promise
+    .all([uploadPhoto(), createUser()])
+    .then((res) => {
+      console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
+    })
     .catch(() => console.log('Signup system offline'));
 }
