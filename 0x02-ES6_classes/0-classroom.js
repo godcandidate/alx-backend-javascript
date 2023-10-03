137ebed8564cb9082dc558e9db86fa0a9fef7f57
godcandidate <godcandidate@gmail.com>
2023-10-03 13:44:54 +0000

    update 0-classroom.js

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -2,10 +2,11 @@
  * Represents a class room.
  */
 export default class ClassRoom {
-  /**
-   * Creates a new @see {@link ClassRoom}.
-   * @param {Number} maxStudentsSize - The maximum number of
-   * students in the class.
+ /**
+   * Create a class room.
+   * @param {Number} maxStudentsSize - The maximum number.
+   * @param {string} lastName - The last name of the person
+   * of students in the class.
    */
   constructor(maxStudentsSize) {
     this._maxStudentsSize = maxStudentsSize;
