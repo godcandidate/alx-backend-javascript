e63fe3668553cc98fa43a08552f687cf90f48429
godcandidate <godcandidate@gmail.com>
2023-10-03 08:00:02 +0000

    update 3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,6 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
+  
   return Promise
     .all([uploadPhoto(), createUser()])
     .then((res) => {
